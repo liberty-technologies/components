694505cd90333a1804345e1a8c78d2865a404475
Nessim Abadi <abadinessim@gmail.com>
2023-12-30 11:32:47 -0500

    fix tailwind merge usage

--- resources/views/components/links/base.blade.php
+++ resources/views/components/links/base.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ $route }}"
-    {{ $attributes->twMerge(['class' => 'inline-flex items-center px-3 py-1.5 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 justify-center']) }}>
+    {{ $attributes->twMerge('inline-flex items-center px-3 py-1.5 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 justify-center') }}>
     {{ $slot }}
 </a>

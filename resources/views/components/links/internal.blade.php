20ddf8f4e6a0e9cfe93f784d9363dd324c3268c9
Nessim Abadi <abadinessim@gmail.com>
2023-12-30 12:26:11 -0500

    add internal link component

---
+++ resources/views/components/links/internal.blade.php
@@ -0,0 +1,5 @@
+<a class="flex items-center space-x-1 hover:text-info-700 hover:underline hover:decoration-info-600 hover:decoration-2 hover:underline-offset-1 dark:hover:text-info-200 dark:hover:decoration-info-400"
+   href="{{ $route }}">
+    <p>{{ $slot }}</p>
+    <x-heroicon-m-link class="h-4 w-4" />
+</a>

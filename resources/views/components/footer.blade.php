b60344a74c711288835eae8e0088fc1243ca6b56
Nessim Abadi <abadinessim@gmail.com>
2023-12-30 12:35:25 -0500

    add footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer
+    class="bg-white p-2 text-sm font-medium text-gray-500 shadow sm:rounded-lg sm:px-3 dark:bg-gray-700 dark:text-gray-400 flex  justify-around">
+    <div>{{ config('app.name') }} | {{ config('app.owner') }} &copy; {{ date('Y') }}</div>
+    @if(empty(trim($slot)) === false)
+        <div class="text-rigth">{{ $slot }}</div>
+    @endif
+</footer>

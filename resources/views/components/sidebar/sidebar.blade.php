c200e470d181a7d9362b9f17597a06442acbb588
Nessim Abadi <abadinessim@gmail.com>
2024-01-02 16:21:24 -0500

    fix sidebar component

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,8 +1,8 @@
-<aside class="fixed inset-y-0 left-0 top-0 z-20 h-full w-44 flex-shrink-0 flex-col pt-16 print:hidden sm:w-64 xl:flex"
+<aside class="fixed inset-y-0 left-0 top-0 z-20 h-full w-44 flex-shrink-0 flex-col pt-16 print:hidden sm:w-64 xl:flex border-primary-300 bg-primary-200 dark:border-primary-600 dark:bg-primary-700 border-r-2"
        id="sidebar" data-simplebar aria-label="{{ __('Sidebar') }}" x-bind:class="{ 'hidden': !sidebarOpen }"
        x-transition.opacity>
     <nav
-        class="w-full overflow-y-auto border-r-2 border-primary-300 bg-primary-200 px-3 pb-4 pt-5 dark:border-primary-600 dark:bg-primary-700">
+        class="w-full min-h-full overflow-y-auto px-3 pb-4 pt-5 ">
         <ul class="space-y-2">
            {{ $slot }}
         </ul>

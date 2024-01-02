92af8244ec3014fab4abf3867c8ebbc9dbf28820
Nessim Abadi <abadinessim@gmail.com>
2024-01-02 09:34:43 -0500

    add app name to header

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -1,3 +1,5 @@
+@props(['logo'])
+
 <nav
     class="fixed z-30 h-16 w-full border-b-2 border-primary-300 bg-primary-200 print:hidden dark:border-primary-600 dark:bg-primary-700">
     <!-- Primary Navigation Menu -->
@@ -5,9 +7,12 @@
         <div class="flex h-16 justify-between">
             <div class="flex">
                 <!-- Logo -->
-                <div class="flex shrink-0 items-center">
+                <a href="{{ $route }}" class="flex items-center space-x-3 rtl:space-x-reverse">
                     {{ $logo }}
-                </div>
+                    <div class="self-center hidden md:block text-2xl font-semibold whitespace-nowrap dark:text-white">
+                        {{ config('app.name') }}
+                    </div>
+                </a>
             </div>
 
             <div class="ml-6 flex items-center space-x-2">
@@ -21,8 +26,8 @@
                     <button
                         class="inline-flex items-center justify-center rounded-md border border-transparent bg-white p-2 text-sm font-medium leading-4 text-gray-500 shadow-sm hover:text-gray-700 focus:outline-none dark:bg-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                         aria-expanded="true" aria-controls="sidebar" @click="sidebarOpen = !sidebarOpen;">
-                        <x-heroicon-o-bars-3 class="h-4 w-4" x-show="!sidebarOpen" />
-                        <x-heroicon-o-x-mark class="h-4 w-4" x-show="sidebarOpen" x-cloak />
+                        <x-heroicon-o-bars-3 class="h-4 w-4" x-show="!sidebarOpen"/>
+                        <x-heroicon-o-x-mark class="h-4 w-4" x-show="sidebarOpen" x-cloak/>
                     </button>
                 </div>
             </div>

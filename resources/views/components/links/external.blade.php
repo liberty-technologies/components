d765fc610047dbffcf303ae07db82f4dc11f8a27
Nessim Abadi <abadinessim@gmail.com>
2024-01-31 15:59:10 -0500

    add external link component

---
+++ resources/views/components/links/external.blade.php
@@ -0,0 +1,5 @@
+<a class="hover:text-info-700 hover:decoration-info-600 dark:hover:text-info-200 dark:hover:decoration-info-400 flex items-center space-x-1 hover:underline hover:decoration-2 hover:underline-offset-1"
+    href="{{ $route }}">
+    <p>{{ $slot }}</p>
+    <x-heroicon-m-arrow-top-right-on-square class="h-4 w-4" />
+</a>

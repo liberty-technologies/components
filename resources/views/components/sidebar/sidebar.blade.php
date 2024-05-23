4aedcf403a3131c17b047956697347487bed99c6
Nessim Abadi <abadinessim@gmail.com>
2024-05-23 16:22:38 -0500

    lint

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside
-    class="border-primary-300 bg-primary-200 dark:border-primary-600 dark:bg-primary-700 fixed inset-y-0 left-0 top-0 z-20 h-full w-44 flex-shrink-0 flex-col border-r-2 pt-16 print:hidden sm:w-64 xl:flex"
+    class="border-primary-300 bg-primary-200 dark:border-primary-600 dark:bg-primary-700 fixed inset-y-0 left-0 top-0 z-20 h-full w-44 flex-shrink-0 flex-col border-r-2 pt-16 sm:w-64 xl:flex print:hidden"
     id="sidebar" data-simplebar aria-label="{{ __('Sidebar') }}" x-bind:class="{ 'hidden': !sidebarOpen }"
     x-transition.opacity>
     <nav class="min-h-full w-full overflow-y-auto px-3 pb-4 pt-5">

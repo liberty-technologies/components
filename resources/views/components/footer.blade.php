38f674914c8a0a7a53bc9282d7ea51a22d6acfa8
Nessim Abadi <abadinessim@gmail.com>
2024-05-23 16:35:28 -0500

    new footer styling

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,11 @@
 <footer
-    class="flex justify-between bg-white p-2 text-sm font-medium text-gray-500 shadow sm:rounded-lg sm:px-3 dark:bg-gray-700 dark:text-gray-400">
-    <div>{{ config('app.name') }} | {{ config('app.owner') }} &copy; {{ date('Y') }}</div>
-    @if (empty(trim($slot)) === false)
-        <div class="text-rigth">{{ $slot }}</div>
-    @endif
+    class="w-full gap-2 bg-white p-2 text-sm font-medium text-gray-500 shadow sm:rounded-lg sm:px-3 dark:bg-gray-700 dark:text-gray-400">
+    <div class="text-center">{{ config('app.name') }}</div>
+    <hr class="my-2 h-px border-0 bg-gray-200 dark:bg-gray-700">
+    <div class="flex flex-col justify-between gap-1 text-center sm:flex-row">
+        <div class="sm:text-start">{{ config('app.owner') }} &copy; {{ date('Y') }}</div>
+        @if (empty(trim($slot)) === false)
+            <div class="sm:text-end">{{ $slot }}</div>
+        @endif
+    </div>
 </footer>

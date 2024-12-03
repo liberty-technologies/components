7836f35e6ba5e694e75730a36eabaa0b61133030
Nessim Abadi <abadinessim@gmail.com>
2024-12-03 10:05:23 -0500

    Fix sidebar on mobile
    Fixes sidebar flash on mobile when loading page

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside
-    class="border-primary-300 bg-primary-200 dark:border-primary-600 dark:bg-primary-700 fixed inset-y-0 left-0 top-0 z-20 h-full w-44 flex-shrink-0 flex-col border-r-2 pt-16 sm:w-64 xl:flex print:hidden"
-    id="sidebar" data-simplebar aria-label="{{ __('Sidebar') }}" x-bind:class="{ 'hidden': !sidebarOpen }"
-    x-transition.opacity>
+    class="border-primary-300 bg-primary-200 dark:border-primary-600 dark:bg-primary-700 fixed inset-y-0 left-0 top-0 z-20 h-full w-44 flex-shrink-0 -translate-x-full transform flex-col border-r-2 pt-16 transition-transform ease-in-out sm:w-64 xl:flex xl:translate-x-0 print:hidden"
+    id="sidebar" data-simplebar aria-label="{{ __('Sidebar') }}"
+    :class="{ '-translate-x-full': !sidebarOpen, 'translate-x-0': sidebarOpen }">
     <nav class="min-h-full w-full overflow-y-auto px-3 pb-4 pt-5">
         <ul class="space-y-2">
             {{ $slot }}

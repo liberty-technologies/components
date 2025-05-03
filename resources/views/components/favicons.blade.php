58ff9b41c91d4d3f1ef117389c2dbbef523cacce
Nessim Abadi <nessimabadi@gmail.com>
2025-05-03 09:46:53 -0500

    Agregar Favicons blade view

---
+++ resources/views/components/favicons.blade.php
@@ -0,0 +1,6 @@
+<link rel="icon" type="image/png" href="{{ asset('vendor/lt-components/icons/favicon-96x96.png') }}" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="{{ asset('vendor/lt-components/icons/favicon.svg') }}" />
+<link rel="shortcut icon" href="{{ asset('vendor/lt-components/icons/favicon.ico') }}" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendor/lt-components/icons/apple-touch-icon.png') }}" />
+<meta name="apple-mobile-web-app-title" content="Liberty" />
+<link rel="manifest" href="{{ asset('vendor/lt-components/icons/site.webmanifest') }}" />

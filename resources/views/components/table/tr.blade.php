eac55e5782c16d63f28cb5d0af228167ab469762
Nessim Abadi <nessimabadi@gmail.com>
2025-05-03 09:58:57 -0500

    Update table row component to add specific border color
    Added "border-b-gray-200" to ensure a consistent border color for light mode. This adjustment improves visual clarity and better aligns with design expectations. Fixes for Tailwind 4

--- resources/views/components/table/tr.blade.php
+++ resources/views/components/table/tr.blade.php
@@ -1,3 +1,4 @@
-<tr {{ $attributes->twMerge('border-b bg-white last-of-type:rounded-b-xl dark:border-gray-700 dark:bg-gray-900') }}>
+<tr
+    {{ $attributes->twMerge('border-b border-b-gray-200 bg-white last-of-type:rounded-b-xl dark:border-gray-700 dark:bg-gray-900') }}>
     {{ $slot }}
 </tr>

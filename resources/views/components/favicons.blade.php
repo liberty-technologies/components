a371569f4a875294891432bfa278c7e751504750
Nessim Abadi <nessimabadi@gmail.com>
2025-05-03 09:58:34 -0500

    Update favicon link attributes for consistency and readability
    Reordered and adjusted attributes in favicon links to improve structure and ensure consistency. This helps with readability and adheres to common HTML conventions. No functionality changes were introduced.

--- resources/views/components/favicons.blade.php
+++ resources/views/components/favicons.blade.php
@@ -1,6 +1,6 @@
-<link rel="icon" type="image/png" href="{{ asset('vendor/lt-components/icons/favicon-96x96.png') }}" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="{{ asset('vendor/lt-components/icons/favicon.svg') }}" />
-<link rel="shortcut icon" href="{{ asset('vendor/lt-components/icons/favicon.ico') }}" />
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendor/lt-components/icons/apple-touch-icon.png') }}" />
+<link type="image/png" href="{{ asset('vendor/lt-components/icons/favicon-96x96.png') }}" rel="icon" sizes="96x96" />
+<link type="image/svg+xml" href="{{ asset('vendor/lt-components/icons/favicon.svg') }}" rel="icon" />
+<link href="{{ asset('vendor/lt-components/icons/favicon.ico') }}" rel="shortcut icon" />
+<link href="{{ asset('vendor/lt-components/icons/apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180" />
 <meta name="apple-mobile-web-app-title" content="Liberty" />
-<link rel="manifest" href="{{ asset('vendor/lt-components/icons/site.webmanifest') }}" />
+<link href="{{ asset('vendor/lt-components/icons/site.webmanifest') }}" rel="manifest" />

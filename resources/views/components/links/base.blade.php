d1f83caa11fd376eef2a3d0ab2c6a2ee48dc2e08
Nessim Abadi <nessimabadi@gmail.com>
2025-07-21 17:11:19 -0500

    Add route and object props to the base link component to prevent variables overflow

--- resources/views/components/links/base.blade.php
+++ resources/views/components/links/base.blade.php
@@ -1,3 +1,5 @@
+@props(['route', 'object'])
+
 <a href="{{ $route }}"
     {{ $attributes->twMerge('inline-flex items-center px-3 py-1.5 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 justify-center') }}>
     {{ $slot }}

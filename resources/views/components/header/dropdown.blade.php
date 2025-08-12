515ec7f85e1014b47a66110fb1f8caa024b8190b
Nessim Abadi <nessimabadi@gmail.com>
2025-08-12 17:22:19 -0500

    Update the dropdown blade component to use Tailwind's `ring-black/5` for consistent opacity handling

--- resources/views/components/header/dropdown.blade.php
+++ resources/views/components/header/dropdown.blade.php
@@ -31,7 +31,7 @@
         x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100"
         x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95" @click="open = false">
-        <div class="{{ $contentClasses }} rounded-md ring-1 ring-black ring-opacity-5">
+        <div class="{{ $contentClasses }} rounded-md ring-1 ring-black/5">
             {{ $content }}
         </div>
     </div>

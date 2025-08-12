906875d1e5fba1e48dfeb6bda8909c7a75293c9c
Nessim Abadi <nessimabadi@gmail.com>
2025-08-12 17:18:56 -0500

    Update Prettier config to adjust Tailwind stylesheet reference
    Replaced `tailwindConfig` with `tailwindStylesheet` pointing to `./resources/css/app.css` for improved project alignment and configuration clarity.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,6 @@
 // prettier.config.js
 module.exports = {
-    "tailwindConfig": "tailwind.config.js",
+    "tailwindStylesheet": "./resources/css/app.css",
     "sortHtmlAttributes": "code-guide",
     "sortTailwindcssClasses": true,
     "plugins": [

ed08e0c98f05f3de3b79ccaef172245c437ac1c3
Nessim Abadi <nessimabadi@gmail.com>
2025-08-12 17:19:14 -0500

    Replace `flex-shrink-0` with `shrink-0` in sidebar components for better Tailwind CSS consistency and adherence to latest conventions.

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -4,7 +4,7 @@
         'bg-primary-700 text-primary-100 text-white shadow-sm dark:bg-primary-100 dark:text-primary-900 fill-primary-100 dark:fill-primary-900' => $active,
         'text-primary-900 dark:text-primary-100 fill-primary-900 dark:fill-primary-100' => !$active,
     ]) {{ $attributes }}>
-        <x-icon class="h-4 w-4 flex-shrink-0 sm:h-6 sm:w-6" :name="$icon" />
+        <x-icon class="h-4 w-4 shrink-0 sm:h-6 sm:w-6" :name="$icon" />
         <span class="ml-2 font-semibold">{{ $slot }}</span>
     </a>
 </li>
